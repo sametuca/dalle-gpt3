4ce4cf2ab9ae592f19a03d5aeca9b8138ce22ae2
Samet UCA <samet.uca@marti.tech>
2022-11-19 19:06:13 +0300

    fix service

--- pages/api/dalleService.js
+++ pages/api/dalleService.js
@@ -1,10 +1,7 @@
 import { Dalle } from "dalle-node";
 
 export default async function handler(req, res) {
-  NProgress.start();
   const dalle = new Dalle(req.query.k);
   const generations = await dalle.generate(req.query.q);
-  console.log(generations);
-  NProgress.done(true);
   res.status(200).json({ result: generations })
 }
